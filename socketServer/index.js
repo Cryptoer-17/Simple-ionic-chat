c79d7d23c1fdbc3f3a9c5ae448f288fe30fb13eb
Andrea17 <andrew.iacoo@gmail.com>
2021-03-10 14:37:52 +0100

    insert server

---
+++ socketServer/index.js
@@ -0,0 +1,31 @@
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+      },
+      allowEIO3: true
+  });
+ 
+io.on('connection', (socket) => {
+ 
+  socket.on('disconnect', function(){
+    io.emit('users-changed', {user: socket.username, event: 'left'});   
+  });
+ 
+  socket.on('set-name', (name) => {
+      console.log('SER NAME: ', name);
+    socket.username = name;
+    io.emit('users-changed', {user: name, event: 'joined'});    
+  });
+  
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, user: socket.username, createdAt: new Date()});    
+  });
+});
+ 
+var port = process.env.PORT || 3001;
+ 
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
